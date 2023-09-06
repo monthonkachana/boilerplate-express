66c3c9ab3f7543180bca8777929aad167cd6c1a9
monthonkachana <maxmonthonkachana2@gmail.com>
2023-09-06 09:36:41 +0700

    all edit

--- myApp.js
+++ myApp.js
@@ -7,17 +7,22 @@ require('dotenv').config()
 // app.get('/',(req,res)=>{
 //   res.send('Hello World')
 // })
+
+//7 Root level logger middleware  ตัวจัดการความผิดพลาด สื่อสาร ระหว่าง อาร์กิวเม้น **check route
+//frist to check root middleware 
+app.use((req, res, next) => {
+    // console.log(`${req.method} ${req.path} ${req.ip} `)
+    console.log(req.method + " " + req.path + " - " + req.ip)
+    next();
+})
 //3 get http file html
-app.get("/", function (req, res) {
+app.get("/", (req, res) => {
     res.sendFile(__dirname + "/views/index.html");
 });
 //4 asste file public open Assets at the /public route
 app.use("/public", express.static(__dirname + "/public"));
-//7 Root level logger middleware  ตัวจัดการความผิดพลาด สื่อสาร ระหว่าง อาร์กิวเม้น **check route
-app.use("/", (req, res, next) => {
-    console.log(`${req.method} ${req.path} ${req.ip} `)
-    next();
-})
+
+
 //5 get file json serve  message 
 app.get('/json', (req, res) => {
     res.json({ "message": "Hello json" })
@@ -29,6 +34,31 @@ app.get('/json', (req, res) => {
     } else {
         res.json({ "message": "Hello json" })
     }
+})
+//8 chian middleeare create time server chian คือ ชื่อ
+app.get('/now', function (req, res, next) {
+    req.time = new Date().toString();
+    next();
+}, function (req, res) {
+    res.send({
+        time: req.time
+    });
+});
+//9 get parameter inpiut from client to ecro
+app.get("/:word/echo", (req, res) => {
+    const { word } = req.params;
+    res.json({
+        echo: word
+    });
+});
+//10 qurey parame input to clinet ecro
+app.get('/name', (req, res) => {
+    var firstname = req.query.frist
+    var lastname = req.query.last
+    var { first: firstName, last: lastName } = req.query;
+    res.json({
+        name: `${firstName} ${lastName}`
+    })
 })
 
 
--- server.js
+++ server.js
@@ -20,8 +20,6 @@ if (!process.env.DISABLE_XORIGIN) {
     next();
   });
 }
-
-
 const port = process.env.PORT || 3000;
 bGround.setupBackgroundApp(app, myApp, __dirname).listen(port, () => {
   bGround.log(`Node is listening on port ${port}...`);
